81d7db76fac6f9c28744ccc06e31df41154a098b
NoaAltsuler <noaalts@gmail.com>
2024-04-20 14:36:53 +0300

    until qestion D

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as np
 from perceptron import Perceptron as pr
-#main
-dataset = [[-2,-1,-1],[0,0,1],[2,1,1],[1,2,1],[-2,2,-1],[-3,0,-1]]
-df = np.DataFrame(dataset,columns=["x1","x2","y"])
-x= df.iloc[:,0:-1]
-y = df.iloc[:,-1]
-perc = pr()
-perc.fit(x,y)
-print(perc.weights_)
+import statsmodels.tools as sm
 
+
+def quesD():
+    dataset = [[-2, -1, -1], [0, 0, 1], [2, 1, 1], [1, 2, 1], [-2, 2, -1], [-3, 0, -1]]
+    df = np.DataFrame(dataset, columns=["x1", "x2", "y"])
+    x = df.iloc[:, 0:-1]
+    y = df.iloc[:, -1]
+    x = sm.add_constant(x)
+    perc = pr()
+    perc.fit(x, y)
+    print("the weights of the data set from question A is:\n",perc.weights_)
+    print("the score is:", perc.score(x,y))
+
+#main
+quesD()
--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,4 @@
 import numpy as np
-import statsmodels.tools as sm
-import pandas as pd
 
 
 class Perceptron:
@@ -8,19 +6,34 @@ class Perceptron:
         self.weights_ = None
 
     def fit(self, x, y):
-        x= sm.add_constant(x)
         self.weights_ = np.zeros(x.shape[1])
         for i in range(0,100):
             i=0
             for gt in y:
-                calc = x.iloc[i,:] @ np.transpose(self.weights_)
-                pred = -1 if calc <0 else 1
-                if(pred < gt):
-                    self.weights_ = np.add(self.weights_, x.iloc[i, :])
+                pred= self.predict(x.iloc[i, :])
+                if (pred < gt):
+                    self.weights_ = np.add(self.weights_, x.iloc[i,:])
                 if (pred > gt):
                     self.weights_ = np.subtract(self.weights_, x.iloc[i, :])
                 i+=1
-        return True
+
+    def predict(self, x):
+        calc = np.dot(x, np.transpose(self.weights_))
+        return -1 if calc < 0 else 1
+
+    def score(self,x,y):
+        count_true =0
+        i=0
+        for gt in y:
+            pred = self.predict(x.iloc[i, :])
+            if pred == gt:
+                count_true+=1
+            i+=1
+        return count_true/y.shape[0]*100
+
+
+
+
 
 
 
