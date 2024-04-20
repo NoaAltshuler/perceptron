f8ac4ccdc88520b2c5d7d17e602ec169311c7e6e
NoaAltsuler <noaalts@gmail.com>
2024-04-20 21:54:50 +0300

    until qestion Duntil qestion D

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ def questionD():
 def questionE():
     print("question E3:\n")
     df = pd.read_csv('Processed Wisconsin Diagnostic Breast Cancer.csv', delimiter= ',')
+    #prepare the data base to precptron
     train_set, test_set = splitDataSet(df)
     x_train_set,y_train_set = splitIntoXandY(train_set)
     x_test_set, y_test_set = splitIntoXandY(test_set)
     y_train_set = zeroToMinosOne(y_train_set)
     y_test_set = zeroToMinosOne(y_test_set)
+
     df_perc = pr()
     df_perc.fit(x_train_set,y_train_set)
     score_test = df_perc.score(x_test_set,y_test_set)
@@ -31,13 +33,17 @@ def questionE():
     print("error rate on training set is:", 1-score_train)
     print("total error rate is:", 1 - 0.8*score_train- 0.2 *score_test)
 
+#split df to paramelts and labels
 def splitIntoXandY(df):
     x,y= df.iloc[:,0:-1],df.iloc[:,-1]
     x = sm.add_constant(x)
     return x,y
+
+# change the zero lable to -1
 def zeroToMinosOne(df):
     df = df.apply(lambda x : -1 if x ==0 else x)
     return df
+# shaffel and split the data to test and train
 def splitDataSet(df):
     df_shaffeld = df.sample(frac= 1 , random_state = 42)
     split_index = int(0.8* len(df_shaffeld))
--- perceptron.py
+++ perceptron.py
@@ -5,20 +5,22 @@ class Perceptron:
     def __init__(self):
         self.weights_ = None
 
-    def fit(self, x, y):
+    def fit(self, x, y,max_itr =100):
+        # init the weights to zero
         self.weights_ = np.zeros(x.shape[1])
-        for i in range(0,100):
+        # go throw the data max itr times
+        for i in range(0,max_itr):
             i=0
             for gt in y:
-                pred= self.predict(x.iloc[i, :])
+                pred= self.predict(x.iloc[i])
                 if (pred < gt):
-                    self.weights_ = np.add(self.weights_, x.iloc[i,:])
+                    self.weights_ = np.add(self.weights_, x.iloc[i])
                 if (pred > gt):
-                    self.weights_ = np.subtract(self.weights_, x.iloc[i, :])
+                    self.weights_ = np.subtract(self.weights_, x.iloc[i])
                 i+=1
 
     def predict(self, x):
-        calc = np.dot(x, np.transpose(self.weights_))
+        calc = np.dot(x, self.weights_)
         return -1 if calc < 0 else 1
 
     def score(self,x,y):
