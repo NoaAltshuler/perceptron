39c2a2975f6a67fbf32ca8dd148b2827e8dfa881
NoaAltsuler <noaalts@gmail.com>
2024-04-20 14:08:27 +0300

    working ver 1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import pandas as np
+from perceptron import Perceptron as pr
 #main
-dataset = [(-2,-1,-1),(0,0,1),(2,1,1),(1,2,1),(-2,2,-1),(-3,0,-1)]
+dataset = [[-2,-1,-1],[0,0,1],[2,1,1],[1,2,1],[-2,2,-1],[-3,0,-1]]
+df = np.DataFrame(dataset,columns=["x1","x2","y"])
+x= df.iloc[:,0:-1]
+y = df.iloc[:,-1]
+perc = pr()
+perc.fit(x,y)
+print(perc.weights_)
+
--- perceptron.py
+++ perceptron.py
@@ -13,9 +13,13 @@ class Perceptron:
         for i in range(0,100):
             i=0
             for gt in y:
-                calc = self.weights_ @ np.transpose(x.iloc[i,:])
+                calc = x.iloc[i,:] @ np.transpose(self.weights_)
                 pred = -1 if calc <0 else 1
-                self.weights_ = np.subtract(self.weights_, (gt-pred)/2*x-x.iloc[i,:])
+                if(pred < gt):
+                    self.weights_ = np.add(self.weights_, x.iloc[i, :])
+                if (pred > gt):
+                    self.weights_ = np.subtract(self.weights_, x.iloc[i, :])
+                i+=1
         return True
 
 
